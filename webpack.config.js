60c2b309f38791f89c76aa911fd8467f843e13c1
alexengrig <alexengrigdev@gmail.com>
2019-02-20 23:02:21 +0300

    Add babel
    Add @babel/core, @babel/preset-env, babel-loader
    Add babel-loader rules to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,19 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
